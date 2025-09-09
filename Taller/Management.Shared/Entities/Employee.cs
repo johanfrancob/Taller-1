22cb5e8a1f27cdcbc2eb7eeea8c10e7b210bb7b6
JOHAN CAMILO FRANCO BEDOYA <johanfranco323357@correo.itm.edu.co>
2025-09-09 12:49:26 -0500

    Employee entity created.

---
+++ Taller/Management.Shared/Entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Shared.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nombre")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres")]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(100, ErrorMessage = "El Apellido no puede superar los 100 caracteres")]
+        [Required(ErrorMessage = "El Apellido es obligatorio.")]
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Salario")]
+        [Required(ErrorMessage = "El salario es obligatorio.")]
+        [Range(0, double.MaxValue, ErrorMessage = "El salario debe minimo es de $1'000.000")]
+        public decimal Salary { get; set; }
+
+    }
+}
